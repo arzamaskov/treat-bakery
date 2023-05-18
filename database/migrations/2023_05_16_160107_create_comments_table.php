b837c0ffd3a11a4f770f6ad55db5302625f528be
Andrey Arzamaskov <arzamaskov@gmail.com>
2023-05-18 13:01:06 +0400

    add `nullable`

--- database/migrations/2023_05_16_160107_create_comments_table.php
+++ database/migrations/2023_05_16_160107_create_comments_table.php
@@ -12,7 +12,7 @@ return new class() extends Migration {
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('parent_id');
+            $table->unsignedBigInteger('parent_id')->nullable();
             $table->string('body');
             $table->morphs('owner');
             $table->timestamps();
