4256e9711b37b9478ea17d1d8b07ead56558f282
Andrey Arzamaskov <arzamaskov@gmail.com>
2023-05-18 13:02:42 +0400

    replase `body` type from `string` to `text`

--- database/migrations/2023_05_16_120648_create_recipes_table.php
+++ database/migrations/2023_05_16_120648_create_recipes_table.php
@@ -15,7 +15,7 @@ return new class() extends Migration {
             $table->id();
             $table->string('slug');
             $table->string('title');
-            $table->string('body');
+            $table->text('body');
             $table->string('image')->nullable();
             $table->foreignIdFor(User::class)
                 ->constrained()
